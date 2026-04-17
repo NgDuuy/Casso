acb7219cbbcd9f20ad0cc3618b35e21b9c6fa86b
Nguyễn Đức Duy <125232468+NgDuuy@users.noreply.github.com>
2026-04-17 18:01:23 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,9 @@ echo "Starting bot..."
 python bot.py &
 
 echo "Starting streamlit..."
+
 streamlit run admin_streamlit.py \
-  --server.port 8501 \
-  --server.address 0.0.0.0 &
+  --server.address 0.0.0.0 \
+  --server.port 8000
 
 wait
